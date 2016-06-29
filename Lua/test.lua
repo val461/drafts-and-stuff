281a647b8498874c4a74d552be86dc8de4166218
fornost <fornost461@users.noreply.github.com>
2016-06-29 23:23:14 +0200

    new function to print solely the keys of a table

--- Lua/test.lua
+++ Lua/test.lua
@@ -17,6 +17,19 @@ function pt(t)  -- print a table shallowly
     return str:sub(1, -3) .. " }"   -- remove last comma
 end
 
+function pk(t)  -- print a table’s keys shallowly
+    local str = ""
+    for k, _ in pairs(t) do
+        if (type(k) == "string") then
+            str = str .. k
+        else
+            str = str .. "[" .. tostring(k) .. "]"
+        end
+        str = str .. ", "
+    end
+    return str:sub(1, -3)   -- remove last comma
+end
+
 function pa(t, withKeys)  -- print an array shallowly
     local str = "{ "
     for i, v in ipairs(t) do
