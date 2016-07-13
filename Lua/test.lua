046f091b52f64679ba9248c524e35b102768820b
fornost <fornost461@users.noreply.github.com>
2016-07-13 20:02:17 +0200

    stuff

--- Lua/test.lua
+++ Lua/test.lua
@@ -48,6 +48,34 @@ function pk(t)  -- print a table’s keys shallowly
     return str .. " }"
 end
 
+function map(f, t)
+    local result = {}
+    for k, v in ipairs(t) do
+        result[k] = f(v)
+    end
+    return result
+end
+
+function newArray(getNewElement, width, height)
+    local result = {}
+    if height and 1 <= height then
+        for i = 1, width do
+            table.insert(
+                result,
+                newArray(
+                    function (j) return getNewElement(i, j) end,
+                    height
+                )
+            )
+        end
+    else
+        for i = 1, width do
+            table.insert(result, getNewElement(i))    -- set default value to false because nil cannot be stored in lua arrays
+        end
+    end
+    return result
+end
+
 function rep(str, n)
     local result = ""
     local i = 0
@@ -56,4 +84,24 @@ function rep(str, n)
         i = i + 1
     end
     return result
+end
+
+function pad(left, right)
+    local result = {}
+    --~ (waiting for monospace font before aligning to the right)
+    --~ local leftColumnSize = sizeOfLongest(left)
+    --~ local rightColumnSize = sizeOfLongest(right)
+    for i, prefix in ipairs(left) do
+        --~ result[prefix] = prefix .. rep(" ", leftColumnSize - #prefix) .. rep(" ", rightColumnSize - #right[i]) .. right[i]
+        result[prefix] = prefix .. "  " .. right[i]
+    end
+    return result
+end
+
+local function length(str)
+    return #str
+end
+
+local function sizeOfLongest(strings)
+    return Tables.max(map(length, strings))
 end
