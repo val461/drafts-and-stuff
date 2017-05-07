ad5785d4cd49e1cf36184ac972a182ce006c12e7
fornost <fornost461@users.noreply.github.com>
2017-05-07 15:56:23 +0200

    stuff

--- Python/startup.py
+++ Python/startup.py
@@ -4,17 +4,17 @@ import pyautogui
 
 #~ exit(1)
 
-pyautogui.PAUSE = 0.5
+pyautogui.PAUSE = 0.2
 
 pyautogui.hotkey('winleft', 'o')
 pyautogui.typewrite('fox')
 pyautogui.press('enter')
 
-pyautogui.hotkey('winleft', '3')
-
 pyautogui.hotkey('winleft', 'o')
 pyautogui.typewrite('sp')
 pyautogui.press('enter')
+
+pyautogui.hotkey('winleft', '2')
 
 pyautogui.hotkey('winleft', 'd')
 pyautogui.typewrite('lu')
@@ -22,14 +22,10 @@ pyautogui.press('enter')
 pyautogui.press('enter')
 pyautogui.press('enter')
 pyautogui.press('enter')
-pyautogui.hotkey('winleft', 'shift', '2')
 
 pyautogui.hotkey('winleft', 'd')
 pyautogui.typewrite('al')
 pyautogui.press('enter')
-pyautogui.hotkey('winleft', 'shift', '2')
-
-pyautogui.hotkey('winleft', '2')
 
 pyautogui.hotkey('winleft', 'e')
 pyautogui.hotkey('winleft', 'e')
