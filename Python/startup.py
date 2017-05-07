a5dec6cd02214537be993dd0290d4b45aaea7af4
fornost <fornost461@users.noreply.github.com>
2017-05-07 09:44:35 +0200

    new script

---
+++ Python/startup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import pyautogui
+
+#~ exit(1)
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.hotkey('winleft', 'o')
+pyautogui.typewrite('fox')
+pyautogui.press('enter')
+
+pyautogui.hotkey('winleft', '3')
+
+pyautogui.hotkey('winleft', 'o')
+pyautogui.typewrite('sp')
+pyautogui.press('enter')
+
+pyautogui.hotkey('winleft', 'd')
+pyautogui.typewrite('lu')
+pyautogui.press('enter')
+pyautogui.press('enter')
+pyautogui.press('enter')
+pyautogui.press('enter')
+pyautogui.hotkey('winleft', 'shift', '2')
+
+pyautogui.hotkey('winleft', 'd')
+pyautogui.typewrite('al')
+pyautogui.press('enter')
+pyautogui.hotkey('winleft', 'shift', '2')
+
+pyautogui.hotkey('winleft', '2')
+
+pyautogui.hotkey('winleft', 'e')
+pyautogui.hotkey('winleft', 'e')
+pyautogui.hotkey('winleft', 'up')
+pyautogui.hotkey('winleft', 'r')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.hotkey('up')
+pyautogui.press('enter')
+
+pyautogui.hotkey('winleft', '1')
