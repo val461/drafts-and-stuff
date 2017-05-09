b96c406d2225dff7ea7c9c986d1d4e81ea896a17
fornost <fornost461@users.noreply.github.com>
2017-05-09 18:42:47 +0200

    edited comment; luminosity and alsamixer now start from startup_1

--- shell/config_files/session/startup_2.py
+++ shell/config_files/session/startup_2.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python3
 
-# launch luminosity and alsamixer
+# set up layout for workspace 2
 
 import pyautogui
-import time
 
-pyautogui.PAUSE = 0.2
+pyautogui.PAUSE = 0.1
 
 pyautogui.hotkey('winleft', '2')
-
-# launch bin/luminosity
-
-pyautogui.hotkey('winleft', 'd')
-time.sleep(4)
-pyautogui.typewrite('lu')
-pyautogui.press('enter')
-
-# launch alsamixer
-
-pyautogui.hotkey('winleft', 'd')
-pyautogui.typewrite('al')
-pyautogui.press('enter')
-
-# set up layout
-
 pyautogui.hotkey('winleft', 'e')
 pyautogui.hotkey('winleft', 'e')
 pyautogui.hotkey('winleft', 'up')
