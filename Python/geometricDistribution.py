87874c77033106f1904478e203f9b23ba8813011
fornost <fornost461@users.noreply.github.com>
2017-05-15 10:42:33 +0200

    pythonization

--- Python/geometricDistribution.py
+++ Python/geometricDistribution.py
@@ -17,23 +17,23 @@ def newList(n):
     return a
 
 def main1(n=1000):
-    # show the distribution of the number of trials necessary for a 6 to appear
+    """Repeat n times the experience newTry(), and show the distribution of the results."""
     l=newList(n)
     u=list(set(l))
     for x in sorted(u, key=l.count, reverse=True):
-        print(x,"\t",l.count(x))
-    print(len(u),"items")
+        print("{}\t{}".format(x,l.count(x)))
+    print("({} items)".format(len(u)))
 
 def p(n):
-    # probability that n trials are necessary for a 6 to appear
+    """Probability that n trials are necessary for a 6 to appear."""
     return (1/6) * (5/6)**(n-1)
 
 def main2():
-    # show that it is more likely than not that at least one 6 appears in one the first four trials
+    """Show that it is more likely than not that at least one 6 appears in one the first four trials."""
     s=0
     i=0
     while s < 1/2:
         i=i+1
         p_i=p(i)
         s+=p_i
-        print(i,s,"\t",p_i)
+        print("{} {}\t{}".format(i,s,p_i))
