06e2aa5e5d7186874de2d2f3ee8b564050b6fb12
fornost <fornost461@users.noreply.github.com>
2017-05-29 18:10:50 +0200

    more helpful comments

--- Python/screenshot.py
+++ Python/screenshot.py
@@ -4,9 +4,9 @@
 
 # Instructions:
 # Put whatever you want to capture on the next wm screen.
-# The result will be saved in the current working directory, so you probably should run
-# this script with the absolute path, e.g. ~/Dev/drafts-and-stuff/Python/screenshot.py
-# You will be asked for the mouse starting and ending positions.
+# The result will be saved in the current working directory, so you probably should
+# run this script with the absolute path, e.g. ~/Dev/drafts-and-stuff/Python/screenshot.py
+# You will be asked for the mouse starting and ending positions (to delimit the capture zone).
 
 import pyautogui, subprocess, time
 
