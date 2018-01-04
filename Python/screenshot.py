f679365073e60f29cd89e8bfc0b5c5f9b2275949
fornost <fornost461@users.noreply.github.com>
2018-01-04 17:04:07 +0100

    stuff

--- Python/screenshot.py
+++ Python/screenshot.py
@@ -6,23 +6,37 @@
 # Put whatever you want to capture on the next wm screen.
 # The result will be saved in the current working directory, so you probably should
 # run this script with the absolute path, e.g. ~/Dev/drafts-and-stuff/Python/screenshot.py
-# You will be asked for the mouse starting and ending positions (to delimit the capture zone),
-# unless you manually edit ‘pos’ prior to running the script.
+# You will be asked for the mouse positions (to delimit the capture zone),
+# unless you manually edit ‘zone’ prior to running the script.
 
 import pyautogui, subprocess, time
 
 print('Adapt the script to the local context before running.'); exit()
 
-pos = []
-#~ pos = [(575, 53), (1109, 836)]
+zone = {}
+# ~ zone = {'left': 954, 'top': 280, 'right': 1451, 'bottom': 679}
 
-if len(pos) != 2:
-    pos = [None, None]
-    for i in range(2):
-        input('Place the mouse in location {} and type enter to validate: '.format(i+1))
-        pos[i] = pyautogui.position()
+directions = ["left","right","top","bottom"]
 
-print(repr(pos))
+if set(zone.keys()) != set(directions):
+    zone = {}
+
+    for d in directions[:2]:
+        try:
+            zone[d] = int(input('{} border (with mouse or keyboard); press enter to validate: '.format(d)))
+        except ValueError:
+            zone[d] = pyautogui.position()[0]
+        print(zone[d])
+
+
+    for d in directions[2:]:
+        try:
+            zone[d] = int(input('{} border (with mouse or keyboard); press enter to validate: '.format(d)))
+        except ValueError:
+            zone[d] = pyautogui.position()[1]
+        print(zone[d])
+
+print(repr(zone))
 
 pyautogui.PAUSE = 0.3
 
@@ -33,12 +47,14 @@ pyautogui.hotkey('winleft', 'tab')
 subprocess.Popen(['scrot', '--select', '--quality', '100'])
 
 # select shooting area
-pyautogui.moveTo(*pos[0])
+i=0
+pyautogui.moveTo(zone[directions[i]], zone[directions[i+2]])
 pyautogui.mouseDown()
-pyautogui.moveTo(*pos[1], 1)
+i=1
+pyautogui.moveTo(zone[directions[i]], zone[directions[i+2]], 0.7)
 
 # give the user some time to ponder over the surreal nature of existence
-time.sleep(3)
+time.sleep(1)
 
 # shoot
 pyautogui.mouseUp()
