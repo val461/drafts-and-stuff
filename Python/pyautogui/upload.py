2c8feaffd9baba91f5975dfd6e2170c05a2ac856
fornost <fornost461@users.noreply.github.com>
2018-04-23 19:01:23 +0200

    s

--- Python/pyautogui/upload.py
+++ Python/pyautogui/upload.py
@@ -6,18 +6,10 @@
 
 import pyautogui, time, sys
 
-if len(sys.argv) <= 1 or sys.argv[1] != '-f':
-    print('Adapt the script to the local context before running.'); exit()
+# ~ if len(sys.argv) <= 1 or sys.argv[1] != '-f':
+    # ~ print('Adapt the script to the local context before running.'); exit()
 
 pyautogui.PAUSE = 1.5
-
-def open_import_menu():
-    pyautogui.click(x=69, y=247)
-    pyautogui.hotkey('down')
-    pyautogui.hotkey('down')
-    pyautogui.hotkey('enter')
-    time.sleep(3)
-    pyautogui.click(x=328, y=249)
 
 # go to web browser
 pyautogui.hotkey('winleft', '1')
@@ -27,11 +19,25 @@ pyautogui.hotkey('down')
 pyautogui.hotkey('enter')
 time.sleep(6)
 
-open_import_menu()
+# open import menu
+pyautogui.click(x=69, y=247)
+pyautogui.hotkey('down')
+pyautogui.hotkey('down')
+pyautogui.hotkey('enter')
+time.sleep(4)
+pyautogui.click(x=328, y=249)
+
 pyautogui.click(x=761, y=495)
 pyautogui.hotkey('enter')
 
-open_import_menu()
+# open import menu
+pyautogui.click(x=69, y=247)
+pyautogui.hotkey('down')
+pyautogui.hotkey('down')
+pyautogui.hotkey('enter')
+time.sleep(2)
+pyautogui.click(x=328, y=249)
+
 pyautogui.click(x=755, y=453)
 pyautogui.hotkey('enter')
 pyautogui.click(x=794, y=273)
