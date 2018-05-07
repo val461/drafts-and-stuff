3c128193f89cca66b0a0d7e11eeaaf971bdd1c1d
fornost <fornost461@users.noreply.github.com>
2018-05-07 22:11:25 +0200

    code made simpler and mode powerful (now takes into account prefilled values even if not all are available); changed order of directions; increased time for the user to check the result

--- Python/pyautogui/screenshot.py
+++ Python/pyautogui/screenshot.py
@@ -6,7 +6,7 @@
 # Put whatever you want to capture on the next wm screen.
 # The result will be saved in the current working directory, so you probably should
 # run this script with the absolute path, e.g. ~/Dev/drafts-and-stuff/Python/screenshot.py
-# You will be asked for the mouse positions (to delimit the capture zone),
+# You will be asked for mouse positions (to delimit the capture zone),
 # unless you manually edit ‘zone’ prior to running the script.
 
 import pyautogui, subprocess, time
@@ -16,19 +16,17 @@ print('Adapt the script to the local context before running.'); exit()
 zone = {}
 # ~ zone = {'left': 954, 'top': 280, 'right': 1451, 'bottom': 679}
 
-directions = ["left","right","top","bottom"]
+directions = ["left","bottom","right","top"]
 
-if set(zone.keys()) != set(directions):
-    zone = {}
-    for d in directions:
-        try:
-            zone[d] = int(input('select {} border (with the mouse or type a number) [enter]'.format(d)))
-            mode = "keyboard"
-        except ValueError:
-            dimension = int(d in ["top","bottom"])
-            zone[d] = pyautogui.position()[dimension]
-            mode = "mouse"
-        print(zone[d], "(selected from {} input)".format(mode))
+for d in set(directions) - set(zone.keys()):
+    try:
+        zone[d] = int(input('select {} border (with the mouse or type a number) [enter]'.format(d)))
+        mode = "keyboard"
+    except ValueError:
+        dimension = int(d in ["top","bottom"])
+        zone[d] = pyautogui.position()[dimension]
+        mode = "mouse"
+    print(zone[d], "(selected from {} input)".format(mode))
 
 
 print(repr(zone))
@@ -43,13 +41,13 @@ subprocess.Popen(['scrot', '--select', '--quality', '100'])
 
 # select shooting area
 i=0
-pyautogui.moveTo(zone[directions[i]], zone[directions[i+2]])
+pyautogui.moveTo(zone[directions[i]], zone[directions[i+1]])
 pyautogui.mouseDown()
-i=1
-pyautogui.moveTo(zone[directions[i]], zone[directions[i+2]], 0.7)
+i=2
+pyautogui.moveTo(zone[directions[i]], zone[directions[i+1]], 0.7)
 
 # give the user some time to ponder over the surreal nature of existence
-time.sleep(1)
+time.sleep(2)
 
 # shoot
 pyautogui.mouseUp()
