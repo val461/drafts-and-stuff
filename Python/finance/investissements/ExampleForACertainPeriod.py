f602c87e6093cc81413c7966674e6b919a3baa28
fornost <fornost461@users.noreply.github.com>
2019-09-03 02:42:03 +0200

    s

--- Python/finance/investissements/ExampleForACertainPeriod.py
+++ Python/finance/investissements/ExampleForACertainPeriod.py
@@ -2,11 +2,21 @@
 
 
 import math
+from decimal import Decimal
+
+
+def GCD(a, b):
+    assert a > 0
+    assert b > 0
+    a, b = map(Decimal, (a, b))
+    while b != 0:
+        a, b = b, a % b
+    return float(a)
 
 
 class ExampleForACertainPeriod:
 
-    def __init__(self, return_rate_per_year, saved_per_month, duration_in_months, opening_balance, fees_rate_per_year, fee_by_contribution, period_size_in_months, account_type = None):
+    def __init__(self, saved_per_month, duration_in_months, opening_balance, withdrawal_fees, portfolio_wanted_percentages, portfolio_return_rates_per_year, rebalance_period_in_months, fees_rate_per_year, fee_by_contribution, period_size_in_months, tick = None, account_type = None):
 
         self.desc = None
         self.duration_in_months = duration_in_months
@@ -15,46 +25,118 @@ class ExampleForACertainPeriod:
         self.opening_balance = opening_balance
         self.fee_by_contribution = fee_by_contribution
         self.account_type = account_type
-        self.return_rate_per_year = return_rate_per_year - fees_rate_per_year
-        self.whole_return_rate_per_year = return_rate_per_year
         self.fees_rate_per_year = fees_rate_per_year
+        self.withdrawal_fees = withdrawal_fees
+        self.saved_per_month = saved_per_month
+        self.portfolio_wanted_percentages = portfolio_wanted_percentages
+        self.portfolio_return_rates_per_year = portfolio_return_rates_per_year
+        self.rebalance_period_in_months = rebalance_period_in_months
 
-        self.return_factor_per_period = (1 + self.return_rate_per_year / 100) ** (self.period_size_in_months / 12)
+        if tick:
+            self.tick = tick
+        elif self.rebalance_period_in_months:
+            self.tick = GCD(self.period_size_in_months, self.rebalance_period_in_months)
+        else:
+            self.tick = self.period_size_in_months
+
+        self.tick_in_years = self.tick / 12
         self.duration_in_periods = duration_in_months / self.period_size_in_months
         self.number_of_complete_periods = math.floor(self.duration_in_periods)
         self.recurrent_contribution = saved_per_month * self.period_size_in_months
 
+        self.portfolio_return_factors_per_tick = [(1 + (return_rate_per_year - self.fees_rate_per_year) / 100) ** self.tick_in_years for return_rate_per_year in portfolio_return_rates_per_year]
+
         self.balance = opening_balance
-        for _ in range(self.number_of_complete_periods):
-            self.balance = self.balance * self.return_factor_per_period + self.recurrent_contribution - fee_by_contribution
+        self.balances = [p / 100 * opening_balance for p in portfolio_wanted_percentages]
+        self.rebalancing_fees = 0
+        # print()
+        # print()
+        # print(' '.join(f'{b:7.2f}' for b in self.balances))
+        recurrent_net_contribution = self.recurrent_contribution - fee_by_contribution
+        months_since_last_rebalance = self.tick
+        months_since_last_contribution = self.tick
+        month_counter = self.tick
+        while month_counter <= self.duration_in_months:
+            for i in range(len(self.balances)):
+                # returns
+                self.balances[i] = self.balances[i] * self.portfolio_return_factors_per_tick[i]
+            if self.rebalance_period_in_months and months_since_last_rebalance >= self.rebalance_period_in_months:
+                # rebalancing
+                # print('rebalancing')
+                self.balance = sum(self.balances)
+                # print(' '.join(f'{self.balances[i] / self.balance * 100:4.1f}' for i in range(len(self.balances))))
+                # print(' '.join(f'{b:7.2f}' for b in self.balances))
+                self.balance = sum(self.balances) - fee_by_contribution * len(self.balances)
+                for i in range(len(self.balances)):
+                    self.balances[i] = portfolio_wanted_percentages[i] / 100 * self.balance
+                # print(' '.join(f'{self.balances[i] / self.balance * 100:4.1f}' for i in range(len(self.balances))))
+                # print(' '.join(f'{b:7.2f}' for b in self.balances))
+                self.rebalancing_fees += fee_by_contribution * len(self.balances)
+                months_since_last_rebalance = 0
+            if months_since_last_contribution >= self.period_size_in_months:
+                # contribution 
+                for i in range(len(self.balances)):
+                    self.balances[i] += portfolio_wanted_percentages[i] / 100 * recurrent_net_contribution
+                months_since_last_contribution = 0
+            month_counter += self.tick
+            months_since_last_contribution += self.tick
+            months_since_last_rebalance += self.tick
+            # print(f'month_counter {month_counter} '+' '.join(f'{b:7.2f}' for b in self.balances))
+        # print()
+        self.balance = sum(self.balances)
 
         self.contributions = self.recurrent_contribution * self.number_of_complete_periods
-        self.fees = fee_by_contribution * self.number_of_complete_periods
-        self.invested = opening_balance + self.contributions - self.fees
+        self.not_a_divisor = self.contributions < saved_per_month * duration_in_months
+        self.contribution_fees = fee_by_contribution * self.number_of_complete_periods
+        self.fees_except_rebalancing_fees = self.contribution_fees + self.withdrawal_fees
+        self.invested = opening_balance + self.contributions - self.contribution_fees
         self.returns = self.balance - self.invested
-        self.net = self.returns - self.fees
-        self.total_return_rate = self.net / self.invested * 100
-        self.annualized = ((1 + self.total_return_rate / 100) ** (1 / self.duration_in_years) - 1) * 100
 
-        # post tax PEA after at least 5 years
-        self.net_post_tax_PEA = self.net * (100 - 17.2) / 100
-        self.total_return_rate_post_tax_PEA = self.net_post_tax_PEA / self.invested * 100
-        self.annualized_post_tax_PEA = ((1 + self.total_return_rate_post_tax_PEA / 100) ** (1 / self.duration_in_years) - 1) * 100
+        self.net = self.returns - self.fees_except_rebalancing_fees
+        if not self.account_type:
+            self.total_return_rate = self.net / self.invested * 100
+            self.annualized = ((1 + self.total_return_rate / 100) ** (1 / self.duration_in_years) - 1) * 100
+        elif self.account_type == 'PEA':
+            # post tax PEA after at least 5 years
+            # assert self.duration_in_years >= 5
+            # if self.duration_in_years < 5:
+                # print('warning: duration smaller than 5 years, so PEA taxes may be understimated')
+            self.net = self.returns * (100 - 17.2) / 100 - self.fees_except_rebalancing_fees
+            self.total_return_rate = self.net / self.invested * 100
+            self.annualized = ((1 + self.total_return_rate / 100) ** (1 / self.duration_in_years) - 1) * 100
+        else:
+            assert self.account_type == 'AV'
+            # post tax assurance vie after at least 8 years
+            # assert self.duration_in_years >= 8
+            # if self.duration_in_years < 8:
+                # print('warning: duration smaller than 8 years, so AV taxes may be understimated')
+            if self.returns <= 4600:
+                self.net = self.returns * (100 - 17.2) / 100 - self.fees_except_rebalancing_fees
+            else:
+                # very roughly
+                self.net = self.returns * (100 - 17.2 - 7.5) / 100 - self.fees_except_rebalancing_fees
+            self.total_return_rate = self.net / self.invested * 100
+            self.annualized = ((1 + self.total_return_rate / 100) ** (1 / self.duration_in_years) - 1) * 100
 
-        # post tax assurance vie after at least 8 years and if net <= 4600 €
-        self.net_post_tax_AV = self.net * (100 - 17.2) / 100
-        self.total_return_rate_post_tax_AV = self.net_post_tax_AV / self.invested * 100
-        self.annualized_post_tax_AV = ((1 + self.total_return_rate_post_tax_AV / 100) ** (1 / self.duration_in_years) - 1) * 100
-        
 
- 
     def __str__(self):
         if not self.desc:
-            if self.account_type == None:
-                self.desc = f'{self.recurrent_contribution:6.1f} contributed every {self.period_size_in_months:4} month(s), net {self.net:7.2f}, annualized {self.annualized:.2f} %, total {self.total_return_rate:.2f} %, returns {self.returns:7.2f}, total fees {self.fees:6.2f}, end balance {self.balance:.2f}, invested {self.invested:.0f}, end balance - fees {self.balance - self.fees:.2f}'
-            elif self.account_type == 'PEA':
-                self.desc = f'PEA: {self.recurrent_contribution:6.1f} contributed every {self.period_size_in_months:4} month(s), net {self.net_post_tax_PEA:7.2f}, annualized {self.annualized_post_tax_PEA:.2f} %, total {self.total_return_rate_post_tax_PEA:.2f} %, returns {self.returns:7.2f}, total fees {self.fees:6.2f}, end balance {self.balance:.2f}, invested {self.invested:.0f}, contributions {self.contributions:.2f}'
-            else:
-                assert(self.account_type == 'AV')
-                self.desc = f' AV: {self.recurrent_contribution:6.1f} contributed every {self.period_size_in_months:4} month(s), net {self.net_post_tax_AV:7.2f}, annualized {self.annualized_post_tax_AV:.2f} %, total {self.total_return_rate_post_tax_AV:.2f} %, returns {self.returns:7.2f}, total fees {self.fees:6.2f}, end balance {self.balance:.2f}, invested {self.invested:.0f}, contributions {self.contributions:.2f}'
+            type_string = ''
+            divisor_string = ''
+            rebalancing_string = ''
+            if self.account_type:
+                type_string = f'{self.account_type}: '
+            if self.not_a_divisor:
+                divisor_string = f', contributions {self.contributions}'
+            if self.rebalance_period_in_months:
+                rebalancing_string = f', rebalance every {self.rebalance_period_in_months:2} month(s)'
+
+            self.desc = f'{type_string}{self.recurrent_contribution:6.1f} contributed every {self.period_size_in_months:4} month(s){rebalancing_string}, net {self.net:7.2f}, annualized {self.annualized:5.2f} %, total {self.total_return_rate:.2f} %, returns {self.returns:7.2f}, total fees {self.fees_except_rebalancing_fees + self.rebalancing_fees:6.2f}, end balance {self.balance:.2f}, invested {self.invested:.0f}, end balance - fees {self.balance - self.fees_except_rebalancing_fees:.2f}{divisor_string}'
+
         return self.desc
+
+
+    def __repr__(self):
+        attributes = [a.split(' = ')[0] for a in 'saved_per_month, duration_in_months, opening_balance, withdrawal_fees, portfolio_wanted_percentages, portfolio_return_rates_per_year, rebalance_period_in_months, fees_rate_per_year, fee_by_contribution, period_size_in_months, tick = None, account_type = None'.split(', ')]
+        s = ', '.join([f'{attribute} = {getattr(self, attribute)!r}' for attribute in attributes])
+        return f'ExampleForACertainPeriod({s})'
--- Python/finance/investissements/recurring_investment.py
+++ Python/finance/investissements/recurring_investment.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from ExampleForACertainPeriod import ExampleForACertainPeriod
+
+from ExampleForACertainPeriod import ExampleForACertainPeriod, GCD
 
 
 def divisors_of(n):
-    assert(n >= 1)
+    assert n >= 1
     return [d for d in range(1, n + 1) if n % d == 0]
 
 
@@ -15,50 +16,98 @@ def divisors_of(n):
 account_type = 'PEA'
 # account_type = 'AV'
 saved_per_month = 25            # currency amount
+# saved_per_month = 0            # currency amount
 duration_in_months = 12*8
-return_rate_per_year = 10       # percentage
-opening_balance = 1000          # currency amount    
+# duration_in_months = 12
+opening_balance = 2000          # currency amount
+# opening_balance = 100          # currency amount
+
+
+# I think in this simulator, you should stick to a single product in the portfolio.
+# Indeed, without rebalancing, using several products in the portfolio is likely to lead to an unbalanced portfolio.
+
+# percentages
+portfolio_wanted_percentages = [100]
+portfolio_return_rates_per_year = [10]
+# portfolio_wanted_percentages = [90, 10]
+# portfolio_return_rates_per_year = [10, 3]
+
 # period_sizes_to_try = [0.5] + list(range(1, 25))    # in months
-# period_sizes_to_try = [23]    # in months
-period_sizes_to_try = [0.25, 0.5] + divisors_of(duration_in_months)    # in months
 # period_sizes_to_try = [x for x in (0.25 * i for i in range(1, 12)) if duration_in_months % x == 0]    # in months
-# all_periods_are_divisors_of_total_duration = False
-all_periods_are_divisors_of_total_duration = True
+# period_sizes_to_try = [12]    # in months
+period_sizes_to_try = [0.25, 0.5] + divisors_of(duration_in_months)    # in months
+# period_sizes_to_try = divisors_of(duration_in_months)    # in months
+
+
+# This simulator seems too unrealistic for the rebalancing simulation to be useful.
+
+rebalance_periods_to_try = [None]   # do not simulate rebalancing
+# rebalance_periods_to_try = list(range(1, 14))    # in months
+# rebalance_periods_to_try = list(range(1, 14)) + [15, 18, 24, 36, 72, 100]    # in months
+# rebalance_periods_to_try = [1, 3, 6, 12]    # in months
+
+tick = None
+
 show_plot = False
 # show_plot = True
 
-# just_the_final_examples = False
-just_the_final_examples = True
+x_axis = period_sizes_to_try
+# x_axis = rebalance_periods_to_try
 
-if account_type == 'AV':
-    fees_rate_per_year = 0.75       # percentage for AV
-    fee_by_contribution = 0         # currency amount for AV
+show_many_examples = True
+# show_many_examples = False
+
+fees_rate_per_year_PEA = 0          # percentage for PEA
+fee_by_contribution_PEA = 1      # currency amount for PEA
+# fee_by_contribution_PEA = 0      # currency amount for PEA
+# withdrawal_fees_PEA = 6             # currency amount
+withdrawal_fees_PEA = 6+8*12*duration_in_months/12*fee_by_contribution_PEA             # currency amount
+
+fees_rate_per_year_AV = 0.75       # percentage for AV
+fee_by_contribution_AV = 0         # currency amount for AV
+withdrawal_fees_AV = 6             # currency amount
+
+if not account_type:
+    fees_rate_per_year = 0          # percentage
+    fee_by_contribution = 0.99      # currency amount
+    withdrawal_fees = 6             # currency amount
+elif account_type == 'PEA':
+    fees_rate_per_year = fees_rate_per_year_PEA
+    fee_by_contribution = fee_by_contribution_PEA
+    withdrawal_fees = withdrawal_fees_PEA
 else:
-    fees_rate_per_year = 0          # percentage for PEA
-    fee_by_contribution = 0.99      # currency amount for PEA
+    assert account_type == 'AV'
+    fees_rate_per_year = fees_rate_per_year_AV
+    fee_by_contribution = fee_by_contribution_AV
+    withdrawal_fees = withdrawal_fees_AV
 
 
 # Code
 
-print(f'maximal possible total contributions (including fees) {saved_per_month * duration_in_months}\n')
-# (normally, amount reached only when period size is a divisor of the total duration)
+print(f'expected total contributions (including fees) {saved_per_month * duration_in_months}\n')
+# (normally, amount reached when period size is a divisor of the total duration)
 
-if just_the_final_examples:
-    chosen_example_for_PEA = ExampleForACertainPeriod(return_rate_per_year, saved_per_month, duration_in_months, opening_balance = 1000, fees_rate_per_year = 0, fee_by_contribution = 0.99, period_size_in_months = 3, account_type = 'PEA')
-    chosen_example_for_AV = ExampleForACertainPeriod(return_rate_per_year, saved_per_month, duration_in_months, opening_balance = 1000, fees_rate_per_year = 0.75, fee_by_contribution = 0, period_size_in_months = 0.25, account_type = 'AV')
-    print(chosen_example_for_PEA)
-    print(chosen_example_for_AV)
-else:
+if show_many_examples:
     examples = []
     for period_size_in_months in period_sizes_to_try:
-        examples.append(ExampleForACertainPeriod(return_rate_per_year, saved_per_month, duration_in_months, opening_balance, fees_rate_per_year, fee_by_contribution, period_size_in_months, account_type))
-        print(examples[-1])
+        for rebalance_period_in_months in rebalance_periods_to_try:
+            examples.append(ExampleForACertainPeriod(saved_per_month, duration_in_months, opening_balance, withdrawal_fees, portfolio_wanted_percentages, portfolio_return_rates_per_year, rebalance_period_in_months, fees_rate_per_year, fee_by_contribution, period_size_in_months, tick, account_type))
+            print(examples[-1])
 
     # best = max(examples, key = lambda example: example.net)
-    best = max(examples, key = lambda example: example.net_post_tax_PEA)
-    print(f'\nbest:\n\n{best}')
+    best = max(examples, key = lambda example: example.net)
+    print(f'\nbest:\n\n{best}\n\nrepr: {best.__repr__()}')
 
     if show_plot:
         import matplotlib.pyplot as plt
-        plt.plot(period_sizes_to_try, [x.net for x in examples])
+        plt.plot(x_axis, [x.net for x in examples])
         plt.show()
+else:
+    # chosen_example_for_PEA = ExampleForACertainPeriod(saved_per_month, duration_in_months, opening_balance, withdrawal_fees, portfolio_wanted_percentages, portfolio_return_rates_per_year, rebalance_period_in_months, fees_rate_per_year = fees_rate_per_year_PEA, fee_by_contribution = fee_by_contribution_PEA, period_size_in_months = 3, tick = tick, account_type = 'PEA')
+
+    chosen_example_for_PEA = ExampleForACertainPeriod(saved_per_month = 25, duration_in_months = 96, opening_balance = 2000, withdrawal_fees = 6, portfolio_wanted_percentages = [90, 10], portfolio_return_rates_per_year = [10, 3], rebalance_period_in_months = rebalance_period_in_months, fees_rate_per_year = 0, fee_by_contribution = 0, period_size_in_months = 0.25, tick = tick, account_type = 'PEA')
+
+    chosen_example_for_AV = ExampleForACertainPeriod(saved_per_month, duration_in_months, opening_balance, withdrawal_fees, portfolio_wanted_percentages, portfolio_return_rates_per_year, rebalance_period_in_months, fees_rate_per_year = fees_rate_per_year_AV, fee_by_contribution = fee_by_contribution_AV, period_size_in_months = 0.25, tick = tick, account_type = 'AV')
+
+    print(chosen_example_for_PEA)
+    print(chosen_example_for_AV)
