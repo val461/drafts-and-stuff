4c89608be4e4453d1432bf6fc226e658cea78856
fornost <fornost461@users.noreply.github.com>
2020-01-03 22:37:45 +0100

    s

--- Python/K.py
+++ Python/K.py
@@ -23,17 +23,34 @@ def chrono(duration = 1, quiet = False, separator = ' '):
 
 class Exercise:
 
-    def __init__(self, hold_duration = 1, rest_duration = 1, repetitions = 1, hold_increment = 1, rest_increment = 1, repetitions_increment = 1, name = None):
+    def __init__(self, hold_duration = 1, rest_duration = 1, repetitions = 1, hold_increment = 0, rest_increment = 0, repetitions_increment = 1, use_chrono = True, name = None):
         self.hold_duration = hold_duration
         self.rest_duration = rest_duration
-        self.repetitions = repetitions
+        self.repetitions = int(repetitions)
         self.hold_increment = hold_increment
         self.rest_increment = rest_increment
         self.repetitions_increment = repetitions_increment
+        self.use_chrono = use_chrono
         if name:
             self.name = name
         else:
             self.name = type(self).__name__
+
+
+    @classmethod
+    def new_from_template(cls, template):
+        '''template: 'Quick', 'Medium', or 'Long'
+        '''
+        result = None
+        if template == 'Quick':
+            result = cls(hold_duration = 1, rest_duration = 1, repetitions = 10, hold_increment = 0, rest_increment = 0, repetitions_increment = 1, use_chrono = False, name = template)
+        elif template == 'Medium':
+            result = cls(hold_duration = 4, rest_duration = 2, repetitions = 5, hold_increment = 0, rest_increment = 0, repetitions_increment = 1, use_chrono = True, name = template)
+        elif template == 'Long':
+            result = cls(hold_duration = 10, rest_duration = 5, repetitions = 5, hold_increment = 1, rest_increment = 0.5, repetitions_increment = 0, use_chrono = True, name = template)
+        else:
+            raise ValueError
+        return result
 
 
     def __str__(self, verbose = False):
@@ -42,85 +59,56 @@ class Exercise:
         else:
             return f'{self.name}\t(H {self.hold_duration:2},  R {self.rest_duration:2},  T {self.repetitions:2})'
 
+
     def __repr__(self):
-        return f'{type(self).__name__}(hold_duration = {self.hold_duration}, rest_duration = {self.rest_duration}, repetitions = {self.repetitions}, hold_increment = {self.hold_increment}, rest_increment = {self.rest_increment}, repetitions_increment = {self.repetitions_increment}, name = {self.name.__repr__()})'
+        return f'Exercise(hold_duration = {self.hold_duration}, rest_duration = {self.rest_duration}, repetitions = {self.repetitions}, hold_increment = {self.hold_increment}, rest_increment = {self.rest_increment}, repetitions_increment = {self.repetitions_increment}, use_chrono = {self.use_chrono}, name = {self.name.__repr__()})'
+
 
     def perform(self):
-        print(f'\nNext exercise: {self}.\nReady? [enter]')
-        input()
-        for r in range(1, self.repetitions + 1):
-            print(f'Hold.')
-            chrono(self.hold_duration)
-            print('Rest.')
-            chrono(self.rest_duration)
-
-
-    def increase_difficulty(self):
-        self._change_difficulty(self.repetitions_increment)
-
-
-    def decrease_difficulty(self):
-        self._change_difficulty(-self.repetitions_increment)
-
-
-    def _change_difficulty(self, difference):
-        print(f'Previous:\t{self}.')
-        new = self.repetitions + difference
-        if new > 0:
-            self.repetitions = new
-        else:
-            print(f'Did not decrease (too low).')
-        print(f'New:\t\t{self}.')
-
-
-class Quick(Exercise):
-
-    def __init__(self, hold_duration = 1, rest_duration = 1, repetitions = 10, *args, **kw_args):
-        super().__init__(hold_duration = hold_duration, rest_duration = rest_duration, repetitions = repetitions, *args, **kw_args)
-
-    def perform(self):
-        print(f'\nNext exercise: {self}.\nYou are supposed to press <enter> each time you release.\nReady? [enter]')
-        input()
-        for r in range(1, self.repetitions + 1):
-            print(f'Hold and release.')
+        print(f'Next exercise: {self}.\n')
+        if self.use_chrono:
+            print(f'Ready? [enter]')
             input()
-
-
-class Medium(Exercise):
-
-    def __init__(self, hold_duration = 4, rest_duration = 2, repetitions = 5, *args, **kw_args):
-        super().__init__(hold_duration = hold_duration, rest_duration = rest_duration, repetitions = repetitions, *args, **kw_args)
-
-
-class Long(Exercise):
-
-    def __init__(self, hold_duration = 10, rest_duration = 5, repetitions = 5, hold_increment = 2, rest_increment = 1, *args, **kw_args):
-        super().__init__(hold_duration = hold_duration, rest_duration = rest_duration, repetitions = repetitions, hold_increment = hold_increment, rest_increment = rest_increment, *args, **kw_args)
+            for r in range(1, self.repetitions + 1):
+                print(f'Hold. ({r} / {self.repetitions})')
+                chrono(self.hold_duration)
+                print('Rest.')
+                chrono(self.rest_duration)
+        else:
+            print(f'Press <enter> each time you release.\nReady? [enter]')
+            input()
+            for r in range(1, self.repetitions + 1):
+                print(f'Hold and release. ({r} / {self.repetitions})')
+                input()
 
 
     def increase_difficulty(self):
-        print(f'Previous:\t{self}.')
-        if self.hold_duration < 20:
-            self.hold_duration += self.hold_increment
-            self.rest_duration += self.rest_increment
-        else:
-            print(f'Did not increase (too high).')
-        print(f'New:\t\t{self}.')
+        self._change_difficulty(1)
 
 
     def decrease_difficulty(self):
+        self._change_difficulty(-1)
+
+
+    def _change_difficulty(self, factor = 1):
+        '''factor: scales increment.'''
         print(f'Previous:\t{self}.')
-        new_hold = self.hold_duration - self.hold_increment
-        new_rest = self.rest_duration - self.rest_increment
-        if new_hold > 0 and new_rest > 0:
+        new_hold = self.hold_duration + factor * self.hold_increment
+        new_rest = self.rest_duration + factor * self.rest_increment
+        new_repetitions = int(self.repetitions + factor * self.repetitions_increment)
+        if new_hold <= 0:
+            print(f'Error: hold duration too low.')
+        elif new_hold > 20:
+            print(f'Error: hold duration too high.')
+        elif new_rest <= 0:
+            print(f'Error: rest duration too low.')
+        elif new_repetitions <= 0:
+            print(f'Error: repetitions too low.')
+        else:
             self.hold_duration = new_hold
             self.rest_duration = new_rest
-        else:
-            print(f'Did not decrease (too low).')
+            self.repetitions = new_repetitions
         print(f'New:\t\t{self}.')
-
-
-    _change_difficulty = None
 
 
 class Session:
@@ -131,7 +119,8 @@ class Session:
         if load_from_file:
             self.exercises = self.load(filename)
         if not self.exercises:
-            self.exercises = [Medium(), Quick(), Long()]
+            print('Using default exercises.\n')
+            self.exercises = list(map(Exercise.new_from_template, ['Medium', 'Quick', 'Long']))
 
 
     def __str__(self):
@@ -142,7 +131,6 @@ class Session:
         return '\n'.join(exercise.__repr__() for exercise in self.exercises)
 
 
-    # testme
     def load(self, filename = None):
         exercises = None
         if not filename:
@@ -152,13 +140,14 @@ class Session:
         else:
             try:
                 with open(filename, 'r') as my_file:
-                    exercises = map(eval, my_file.readlines())
+                    exercises = list(map(eval, my_file.readlines()))
             except OSError as error:
-                print(f'Error: failed to load file " {filename} ". ({error}.)')
+                print(f'Warning: failed to load file " {filename} ". ({error}.)\n')
+            else:
+                print(f'Loaded exercises from {filename}.\n')
         return exercises
 
 
-    # testme
     def save(self, filename = None):
         done = False
         if not filename:
@@ -172,32 +161,33 @@ class Session:
                     done = True
                     print(f'Saved to file {filename}.')
             except OSError as error:
-                print(f'Error: failed to save to file " {filename} ". ({error})')
+                print(f'Error: failed to save to file " {filename} ". ({error})\n')
         return done
 
 
     def run(self):
         print(self)
-        for exercise in self.exercises:
+        print()
+        for exercise in self.exercises[1:2]:
+            # print(f'exercise: {exercise}.') #comment me
             exercise.perform()
-            print('[i]ncrease / [d]ecrease difficulty? (Or <enter> to make no changes.)')
+            print('[i]ncrease / [d]ecrease difficulty? (<enter> to make no changes.)')
             answer = input().lower()
             if answer == 'i':
                 exercise.increase_difficulty()
             elif answer == 'd':
                 exercise.decrease_difficulty()
+        print()
         print(self)
-        print('Save? [y]/n')
-        answer = input().lower()
-        if answer == 'n':
-            print('Not saving.')
-        else:
-            print('Saving.')
-            self.save()
+        if self.filename:
+            print('Save? [y]/n')
+            answer = input().lower()
+            if answer == 'n':
+                print('Not saving.')
+            else:
+                self.save()
 
 
 if __name__ == '__main__':
     s = Session(load_from_file = True, filename = 'K_data')
-    # s = Session(filename = 'K_data')
-    # s.save()
     s.run()
