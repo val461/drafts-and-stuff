a54d89a6c2c43994b63bb74fd1c84c458a5b2dbc
fornost <fornost461@users.noreply.github.com>
2020-12-13 23:56:19 +0100

    v

--- Python/pyautogui/screenshot.py
+++ Python/pyautogui/screenshot.py
@@ -4,21 +4,9 @@
 
 '''
 TODO :
-Update shortcuts.
-Add a variable for output file location.
-Make use of scrot arguments.
-    coordinates
-    output file location
-Loop :
-    input
-    if ""
-        screenshot
-    elif "m"
-        set coordinates with mouse position
-    elif "f"
-        set coordinates to fullscreen
-    elif "q"
-        quit
+Fix loop.
+Move parameters (filename, extension, folder) to data file along with region coordinates.
+Make window switching togglable.
 Update instructions.
 '''
 
@@ -26,51 +14,75 @@ Update instructions.
 # Put whatever you want to capture on the next wm screen.
 # The result will be saved in the current working directory, so you probably should
 # run this script with the absolute path, e.g. ~/Dev/drafts-and-stuff/Python/pyautogui/screenshot.py
-# You will be asked for mouse positions (to delimit the capture zone),
-# unless you manually edit ‘zone’ prior to running the script.
+# You will be asked for mouse positions (to delimit the capture region),
+# unless you manually edit ‘region’ prior to running the script.
 
 import pyautogui, subprocess, time
 
 print('Adapt the script to the local context before running.'); exit()
 
-zone = {}
-# ~ zone = {'left': 954, 'top': 280, 'right': 1451, 'bottom': 679}
-
-directions = ["left","bottom","right","top"]
-
-for d in set(directions) - set(zone.keys()):
-    try:
-        zone[d] = int(input('select {} border (with the mouse or type a number) [enter]'.format(d)))
-        mode = "keyboard"
-    except ValueError:
-        dimension = int(d in ["top","bottom"])
-        zone[d] = pyautogui.position()[dimension]
-        mode = "mouse"
-    print(zone[d], "(selected from {} input)".format(mode))
-
-
-print(repr(zone))
+filename='%Y-%m-%d@%H:%M:%S'
+extension='png'
+folder='~/Pictures/screenshots'
 
 pyautogui.PAUSE = 0.3
 
-# go to the next wm screen
-pyautogui.hotkey('winleft', 'tab')
+directions = ["left","bottom","right","top"]
+region = {}
+# region = {'left': 954, 'top': 280, 'right': 1451, 'bottom': 679}
+fullscreen = True
+answer = None
 
-# run scrot
-subprocess.Popen(['scrot', '--select', '--quality', '100'])
+while answer != 'q':
+    answer = input('> ')
+    if answer = ""
+        # take screenshot
 
-# select shooting area
-i=0
-pyautogui.moveTo(zone[directions[i]], zone[directions[i+1]])
-pyautogui.mouseDown()
-i=2
-pyautogui.moveTo(zone[directions[i]], zone[directions[i+1]], 0.7)
+        # go to the next window
+        pyautogui.hotkey('alt', 'tab')
 
-# give the user some time to ponder over the surreal nature of existence
-time.sleep(2)
+        # run scrot
+        if fullscreen:
+            subprocess.Popen(['scrot', f'{folder}/{filename}.{extension}'])
+        else:
+            x = region["left"]
+            y = region["top"]
+            w = region["right"] - region["left"]
+            h = region["bottom"] - region["top"]
+            subprocess.Popen(['scrot', f'-a{x},{y},{w},{h}', f'{folder}/{filename}.{extension}'])
 
-# shoot
-pyautogui.mouseUp()
+        # return to the previous window
+        pyautogui.hotkey('alt', 'tab')
+    elif answer = "f"
+        fullscreen = True
+    elif answer = "r"
+        # FIXME
+        # switch to region mode unless region coordinates are missing
+        fullscreen = False
+    elif answer = "m"
+        # FIXME
+        # set region coordinates
+        for d in set(directions) - set(region.keys()):
+            try:
+                region[d] = int(input('select {} border (with the mouse or type a number) [enter]'.format(d)))
+                source = "keyboard"
+            except ValueError:
+                dimension = int(d in ["top","bottom"])
+                region[d] = pyautogui.position()[dimension]
+                source = "mouse"
+            print(region[d], "(selected from {} input)".format(source))
+        print(repr(region))
+    elif answer = "l"
+        # FIXME
+        # choose region coordinates to load from a data file
+        pass
+    elif answer = "s"
+        # FIXME
+        # save region coordinates in a data file, if with a possibly empty title
+        # data file format: python dictionaries
+        pass
+    elif answer = "q"
+        print('Quitting.')
 
-# go to the previous wm screen
-pyautogui.hotkey('winleft', 'shift', 'tab')
+
+
