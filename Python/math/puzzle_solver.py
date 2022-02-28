8c1c4cd6cc768d943ff57afd5478fc5b2d1a7306
fornost <fornost461@users.noreply.github.com>
2022-02-28 21:50:10 +0100

    -

--- Python/math/puzzle_solver.py
+++ Python/math/puzzle_solver.py
@@ -23,6 +23,9 @@ def emplacements_libres(precube): # TO TEST
                 if (x,y,z) not in emplacements_pris:
                     yield (x,y,z)
 
+def R1(bloc):
+    return bloc
+
 def Rxy(bloc):
     (x,y,z,p) = bloc
     return (-y,x,z,p)
@@ -31,25 +34,22 @@ def Rzy(bloc):
     (x,y,z,p) = bloc
     return (x,z,-y,p)
 
-def iterer(R,n):
-    def iR(bloc):
+def iterer(f,n):
+    def f_new(x):
         for k in range(n):
-            bloc = R(bloc)
-        return bloc
-    return iR
+            x = f(x)
+        return x
+    return f_new
 
 Ryx = iterer(Rxy,3)
 Rzy = iterer(Rzy,3)
-Rv_xpxm = iterer(Rxy,2)
 Rv_ypym = iterer(Rzy,2)
 
+rotations_v = [R1,Rzy,iterer(Rzy,2),iterer(Rzy,3),,] # TO DO
+rotations_h = [R1,Rxy,iterer(Rxy,2),Ryx]
 
 def positions(piece):
     # TO CHECK : Rzx=RyxRzyRxy
-    # TO DO
-    rotations_v =
-    # TO DO
-    rotations_h = 
     for rotation_v in rotations_v:
         for rotation_h in rotations_h:
             yield(rotation_v(rotation_h(piece))) # TO FIX
