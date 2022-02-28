9d1313c671f9dac1a3a6b4083cff68f7d87197ca
fornost <fornost461@users.noreply.github.com>
2022-02-28 21:42:50 +0100

    -

--- Python/math/puzzle_solver.py
+++ Python/math/puzzle_solver.py
@@ -1,34 +1,74 @@
 #!/usr/bin/env python3
 
-precubes = []
+go = False
+precubes = set()
 
-# TO FIX
+# TO DO
 # pieces_restantes = [
-# [(x,y,z,c),
- # (x,y,z,c),
+# [(x,y,z,0),
+ # (x,y,z,0),
 # ],
-# [(x,y,z,c),
- # (x,y,z,c),
+# [(x,y,z,1),
+ # (x,y,z,1),
 # ]
 # ]
 
 # centre d'une pièce : 1er bloc de sa liste
 
-continuer = True
+def emplacements_libres(precube): # TO TEST
+    emplacements_pris = {(x,y,z) for (x,y,z,p) in precube}
+    for x in range(3):
+        for y in range(3):
+            for z in range(3):
+                if (x,y,z) not in emplacements_pris:
+                    yield (x,y,z)
 
-while pieces_restantes and continuer:
-    piece = pop(pieces_restantes) # TO CHECK
-    piece_placee = False
-    new_precubes = []
-    for precube in precubes:
-        for emplacement in precube: # TO FIX
-            for position in positions(piece): # TO FIX
-                if piece fits: # TO FIX
-                    # TO DO
-                    mettre piece dans precube # TO FIX
-                    piece_placee = True
-                    new_precubes.append(precube)
+def Rxy(bloc):
+    (x,y,z,p) = bloc
+    return (-y,x,z,p)
+
+def Rzy(bloc):
+    (x,y,z,p) = bloc
+    return (x,z,-y,p)
+
+def iterer(R,n):
+    def iR(bloc):
+        for k in range(n):
+            bloc = R(bloc)
+        return bloc
+    return iR
+
+Ryx = iterer(Rxy,3)
+Rzy = iterer(Rzy,3)
+Rv_xpxm = iterer(Rxy,2)
+Rv_ypym = iterer(Rzy,2)
+
+
+def positions(piece):
+    # TO CHECK : Rzx=RyxRzyRxy
+    # TO DO
+    rotations_v =
+    # TO DO
+    rotations_h = 
+    for rotation_v in rotations_v:
+        for rotation_h in rotations_h:
+            yield(rotation_v(rotation_h(piece))) # TO FIX
+
+if go:
+    while pieces_restantes and precubes:
+        piece = pieces_restantes.pop()
+        piece_placee = False
+        new_precubes = []
+        for precube in precubes:
+            for emplacement in emplacements_libres(precube):
+                for position in positions(piece):
+                    if fits(piece, precube):
+                        precube |= piece
+                        new_precubes.append(precube)
+                        piece_placee = True
+                        break
+                if piece_placee:
                     break
-            if piece_placee:
-                break
-    precubes = new_precubes
+        precubes = new_precubes
+
+    print(len(precubes),'solution(s).')
