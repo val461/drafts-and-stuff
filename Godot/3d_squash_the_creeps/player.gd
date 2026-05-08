09c2ab94f5b4395ab48967068ebc4376de1b2aa8
val461 <6820182+val461@users.noreply.github.com>
2026-05-08 19:27:26 +0200

    -

--- Godot/3d_squash_the_creeps/player.gd
+++ Godot/3d_squash_the_creeps/player.gd
@@ -1,11 +1,24 @@
 extends CharacterBody3D
 
+# Emitted when the player was hit by a mob.
+signal hit
+
 # How fast the player moves in meters per second.
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+# Vertical impulse applied to the character upon jumping in meters per second.
+@export var jump_impulse = 20
+# Vertical impulse applied to the character upon bouncing over a mob in
+# meters per second.
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
+
+
+func die():
+	hit.emit()
+	queue_free()
 
 
 func _physics_process(delta):
@@ -37,6 +50,38 @@ func _physics_process(delta):
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
+	# Jumping.
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If there are duplicate collisions with a mob in a single frame
+		# the mob will be deleted after the first collision, and a second call to
+		# get_collider will return null, leading to a null pointer when calling
+		# collision.get_collider().is_in_group("mob").
+		# This block of code prevents processing duplicate collisions.
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
+
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+
+func _on_mob_detector_body_entered(body):
+	die()
