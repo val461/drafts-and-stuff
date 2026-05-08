cef10e504fed3686327e4464a80b96410955b21d
val461 <6820182+val461@users.noreply.github.com>
2026-05-08 09:22:28 +0200

    -

--- Godot/dodge-the-creeps/main.gd
+++ Godot/dodge-the-creeps/main.gd
@@ -7,12 +7,19 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -42,6 +49,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
